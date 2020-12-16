5334624ef1f3b727f805726c750d872f7bb8b8d7
grusanov <grusanov@adsterra.com>
2020-12-17 02:19:10 +0300

    add throttle

--- src/test/scala/sushi/SushiInjects.scala
+++ src/test/scala/sushi/SushiInjects.scala
@@ -2,7 +2,7 @@ package sushi
 
 import io.gatling.core.Predef._
 import io.gatling.core.controller.inject.open.IncreasingUsersPerSecCompositeStep
-import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.core.structure.{PopulationBuilder, ScenarioBuilder}
 import io.gatling.http.Predef.http
 import io.gatling.http.protocol.HttpProtocolBuilder
 
@@ -10,7 +10,7 @@ import scala.concurrent.duration._
 
 trait SushiInjects extends Simulation {
 
-  val httpProtocol: HttpProtocolBuilder = http.baseUrl("http://185.177.93.224:3000")
+  val httpProtocol: HttpProtocolBuilder = http.baseUrl("http://185.177.93.224:3000").shareConnections
 
   def constantLoad(
                  first: ScenarioBuilder, firstRps: Int,
@@ -18,11 +18,18 @@ trait SushiInjects extends Simulation {
                  third: ScenarioBuilder, thirdRps: Int
                ): SetUp = {
     val loading = 0.8
+    val loadDuration = FiniteDuration(70, MINUTES)
     setUp(
-      first.inject(constantUsersPerSec((loading * firstRps).toInt) during (70 minutes)).protocols(httpProtocol),
-      second.inject(constantUsersPerSec((loading * secondRps).toInt) during (70 minutes)).protocols(httpProtocol),
-      third.inject(constantUsersPerSec((loading * thirdRps).toInt) during (70 minutes)).protocols(httpProtocol)
-    ).maxDuration(FiniteDuration(80, MINUTES))
+      first.inject(constantUsersPerSec((loading * firstRps).toInt) during loadDuration)
+        .throttle(jumpToRps((loading * firstRps).toInt), holdFor(loadDuration))
+        .protocols(httpProtocol),
+      second.inject(constantUsersPerSec((loading * secondRps).toInt) during loadDuration)
+        .throttle(jumpToRps((loading * secondRps).toInt),holdFor(loadDuration))
+        .protocols(httpProtocol),
+      third.inject(constantUsersPerSec((loading * thirdRps).toInt) during loadDuration)
+        .throttle(jumpToRps((loading * thirdRps).toInt),holdFor(loadDuration))
+        .protocols(httpProtocol)
+    ).maxDuration(loadDuration)
   }
 
   def stairLoad(
@@ -30,20 +37,31 @@ trait SushiInjects extends Simulation {
        second: ScenarioBuilder, secondRps: Int,
        third: ScenarioBuilder, thirdRps: Int
      ): SetUp = {
-    val stepSize = 0.2
-    val steps = 6
     val stepDuration = FiniteDuration(4, MINUTES)
     setUp(
-      first.inject(stairSteps(steps, stepSize, firstRps, stepDuration)).protocols(httpProtocol),
-      second.inject(stairSteps(steps, stepSize, secondRps, stepDuration)).protocols(httpProtocol),
-      third.inject(stairSteps(steps, stepSize, thirdRps, stepDuration)).protocols(httpProtocol)
-    ).maxDuration(FiniteDuration(24, MINUTES))
+      stairThrottle(first.inject(constantUsersPerSec(firstRps * 2) during (stepDuration * 6)), firstRps, stepDuration)
+        .protocols(httpProtocol),
+      stairThrottle(second.inject(constantUsersPerSec(secondRps * 2) during (stepDuration * 6)), secondRps, stepDuration)
+        .protocols(httpProtocol),
+      stairThrottle(third.inject(constantUsersPerSec(thirdRps * 2) during (stepDuration * 6)), thirdRps, stepDuration)
+        .protocols(httpProtocol)
+    ).maxDuration(stepDuration * 6)
   }
 
-  def stairSteps(steps: Int, stepSize: Double, baseRps: Int, stepDur: FiniteDuration) : IncreasingUsersPerSecCompositeStep = {
-    incrementUsersPerSec(baseRps * stepSize)
-      .times(steps)
-      .eachLevelLasting(stepDur)
-      .startingFrom(baseRps)
+  def stairThrottle(pb: PopulationBuilder, baseRps: Int, stepDuration: FiniteDuration) : PopulationBuilder = {
+    pb.throttle(
+      jumpToRps(baseRps),
+      holdFor(stepDuration),
+      jumpToRps((baseRps * 1.2).ceil.toInt),
+      holdFor(stepDuration),
+      jumpToRps((baseRps * 1.4).ceil.toInt),
+      holdFor(stepDuration),
+      jumpToRps((baseRps * 1.6).ceil.toInt),
+      holdFor(stepDuration),
+      jumpToRps((baseRps * 1.8).ceil.toInt),
+      holdFor(stepDuration),
+      jumpToRps(baseRps * 2),
+      holdFor(stepDuration),
+    )
   }
 }
--- src/test/scala/sushi/SushiMethods.scala
+++ src/test/scala/sushi/SushiMethods.scala
@@ -12,16 +12,13 @@ object SushiMethods {
   val mainPage: ChainBuilder = pace(1 seconds)
     .exec(http("mainPage")
       .get("/"))
-    .pause(1)
 
   val getOrders: ChainBuilder = pace(1 seconds)
     .exec(http("get orders")
       .get("/api/orders"))
-    .pause(1)
 
   val takeOrder: ChainBuilder = pace(1 seconds)
     .exec(http("take order")
       .post("/api/orders")
       .body(RawFileBody("orderDetails.json")).asJson)
-    .pause(1)
 }
